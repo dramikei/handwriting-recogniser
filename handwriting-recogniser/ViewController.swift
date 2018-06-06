9eff9c5da09b5c13ddf4df2f9517137e2c040f5c
Raghav Vashisht <raghav2011rv@gmail.com>
2018-06-06 10:30:33 +0530

    Connected outlets, wrote some code

--- handwriting-recogniser/ViewController.swift
+++ handwriting-recogniser/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var predictionLabel: UILabel!
+    
+    //Variables
+    var lastPoint:CGPoint!
+    var isSwiping:Bool!
+    var red:CGFloat!
+    var green:CGFloat!
+    var blue:CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        red = (0.0/255.0)
+        green = (0.0/255.0)
+        blue = (0.0/255.0)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isSwiping = false
+        if let touch = touches.first {
+            lastPoint = touch.location(in: imageView)
+        }
     }
-
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isSwiping = true
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: imageView)
+            UIGraphicsBeginImageContext(imageView.frame.size)
+            imageView.image?.draw(in: CGRect(x: 0, y: 0, width: imageView.frame.width, height: imageView.frame.height))
+            
+        }
+    }
+    
+    
+    @IBAction func crearBtnPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func decideBtnPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
