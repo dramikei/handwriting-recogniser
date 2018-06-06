c2dad7129d40bd89a41f4f6291741113d6e07a49
Raghav Vashisht <raghav2011rv@gmail.com>
2018-06-06 11:12:00 +0530

    completed code for drawing on image view

--- handwriting-recogniser/ViewController.swift
+++ handwriting-recogniser/ViewController.swift
@@ -17,15 +17,9 @@ class ViewController: UIViewController {
     //Variables
     var lastPoint:CGPoint!
     var isSwiping:Bool!
-    var red:CGFloat!
-    var green:CGFloat!
-    var blue:CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        red = (0.0/255.0)
-        green = (0.0/255.0)
-        blue = (0.0/255.0)
         
     }
     
@@ -42,10 +36,36 @@ class ViewController: UIViewController {
             let currentPoint = touch.location(in: imageView)
             UIGraphicsBeginImageContext(imageView.frame.size)
             imageView.image?.draw(in: CGRect(x: 0, y: 0, width: imageView.frame.width, height: imageView.frame.height))
-            
+            let currentContext = UIGraphicsGetCurrentContext()
+            currentContext?.move(to: lastPoint)
+            currentContext?.addLine(to: currentPoint)
+            currentContext?.setLineCap(.round)
+            currentContext?.setLineWidth(9.0)
+            currentContext?.setStrokeColor(UIColor.black.cgColor)
+            currentContext?.setAlpha(1)
+            currentContext?.strokePath()
+            imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            lastPoint = currentPoint
         }
     }
     
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !isSwiping {
+            UIGraphicsBeginImageContext(imageView.frame.size)
+            imageView.image?.draw(in: CGRect(x: 0, y: 0, width: imageView.frame.width, height: imageView.frame.height))
+            let currentContext = UIGraphicsGetCurrentContext()
+            currentContext?.setLineCap(.round)
+            currentContext?.setLineWidth(9.0)
+            currentContext?.setStrokeColor(UIColor.black.cgColor)
+            currentContext?.setAlpha(1)
+            currentContext?.move(to: lastPoint)
+            currentContext?.addLine(to: lastPoint)
+            currentContext?.strokePath()
+            imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+        }
+    }
     
     @IBAction func crearBtnPressed(_ sender: UIButton) {
     }
